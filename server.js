923f5bc4aa37836724cd26cb1713df8504a500c7
Juhi Jain <juhi.jain@nagarro.com>
2019-04-25 16:24:12 +0530

    new commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/Public'));
+app.use(express.static(__dirname + '/public'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080); 
